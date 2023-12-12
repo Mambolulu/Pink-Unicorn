732cf471e0797adb0dbf98907e0f0b662ad39f19
Aaron Olivieri <aaron.olivieri@hotmail.com>
2023-12-12 21:43:50 +0100

    User adjusted
    I'll to fix gitignore in next commit

--- backend/src/main/java/com/example/jwt/domain/user/User.java
+++ backend/src/main/java/com/example/jwt/domain/user/User.java
@@ -37,6 +37,12 @@ public class User extends ExtendedAuditEntity {
   )
   private Set<Role> roles = new HashSet<>();
 
+  @Column(name = "customer_rank")
+  private CustomerRank customerRank;
+
+  @Column(name = "seeds")
+  private int seeds;
+
   public User() {
   }
 
@@ -48,6 +54,8 @@ public class User extends ExtendedAuditEntity {
     this.email = email;
     this.password = password;
     this.roles = roles;
+    this.customerRank;
+    this.seeds;
   }
 
   public String getFirstName() {
@@ -93,5 +101,48 @@ public class User extends ExtendedAuditEntity {
   public User setRoles(Set<Role> roles) {
     this.roles = roles;
     return this;
+  }
+
+  public CustomerRank getCustomerRank() {
+    return customerRank;
+  }
+
+  public void setCustomerRank(CustomerRank customerRank) {
+    this.customerRank = customerRank;
+  }
+
+  public int getSeeds() {
+    return seeds;
+  }
+
+  public void setSeeds(int seeds) {
+    this.seeds = seeds;
+  }
+
+  // Methode zur Berechnung der Seeds basierend auf dem Kaufpreis
+  public void calculateAndAddSeeds(double purchasePrice) {
+    int earnedSeeds = (int) (purchasePrice / 2);
+    this.seeds += earnedSeeds;
+    updateCustomerRank();
+  }
+
+  // Methode zur Aktualisierung des Kundenranges
+  private void updateCustomerRank() {
+    if (this.seeds >= 300) {
+      this.customerRank = CustomerRank.DIAMOND;
+    } else if (this.seeds >= 140) {
+      this.customerRank = CustomerRank.PLATINUM;
+    } else if (this.seeds >= 60) {
+      this.customerRank = CustomerRank.GOLD;
+    } else if (this.seeds >= 20) {
+      this.customerRank = CustomerRank.SILVER;
+    } else if (this.seeds >= 0) {
+      this.customerRank = CustomerRank.BRONZE;
+    }
+  }
+
+  public boolean canPlaceOrder() {
+    return this.roles.stream()
+            .anyMatch(role -> role.getAuthorities().contains(Authority.CAN_PLACE_ORDER));
   }
 }
