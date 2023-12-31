2dd6faf34b28dd6cbfe78b5eff774ee1e22c8891
EliaSchudel <elia.schudel@hotmail.com>
2023-12-31 17:59:54 +0100

    updated

--- backend/src/test/java/com/example/jwt/ProductIntegrationTests.java
+++ backend/src/test/java/com/example/jwt/ProductIntegrationTests.java
@@ -76,45 +76,25 @@ public class ProductIntegrationTests {
 
     @BeforeEach
     public void setUp() {
-        UUID productId1 = UUID.randomUUID();
-        UUID productId2 = UUID.randomUUID();
-        UUID categoryId1 = UUID.randomUUID();
-        UUID categoryId2 = UUID.randomUUID();
-        UUID originId1 = UUID.randomUUID();
-        UUID originId2 = UUID.randomUUID();
-        String variety1 = "Test Variety 1";
-        String variety2 = "Test Variety 2";
-        Category category1 = new Category();
-        category1.setId(categoryId1);
-        category1.setName("Test Category 1");
-        Category category2 = new Category();
-        category2.setId(categoryId2);
-        category2.setName("Test Category 1");
-        Origin origin1 = new Origin();
-        origin1.setId(originId1);
-        origin1.setCountry("Test Country 1");
-        Origin origin2 = new Origin();
-        origin2.setId(originId2);
-        origin2.setCountry("Test Country 2");
-        BigDecimal purchasePrice = new BigDecimal("10.00");
-        BigDecimal sellingPrice = new BigDecimal("15.00");
-        LocalDate harvestDate = LocalDate.now();
-        int stock = 100;
     }
 
     @Test
     public void retrieveAll_requestAllProducts_expectAllProductsAsDTOS() throws Exception {
-        Authority authority = authorityRepository.saveAndFlush(new Authority().setName("CAN_RETRIEVE_PRODUCTS"));
-        Role role = roleRepository.saveAndFlush(new Role().setName("ROLE_TESTER").setAuthorities(Set.of(authority)));
+        Authority authority = authorityRepository.saveAndFlush(new Authority().setName("CAN_RETRIEVE_ALL"));
+        Role role = roleRepository.saveAndFlush(new Role().setName("CLIENTS").setAuthorities(Set.of(authority)));
         User user = userRepository.saveAndFlush(new User().setEmail("john@doe.com").setRoles(Set.of(role)));
-        Category category1 = new Category(UUID.randomUUID(), "New Category 1");
-        Category category2 = new Category(UUID.randomUUID(), "New Category 2");
-        Origin origin1 = new Origin(UUID.randomUUID(), "New Origin 1");
-        Origin origin2 = new Origin(UUID.randomUUID(), "New Origin 2");
+        Category category1 = new Category();
+        category1.setName("New Category 1");
+        Category category2 = new Category();
+        category1.setName("New Category 2");
+        Origin origin1 = new Origin();
+        origin1.setCountry("New Origin 1");
+        Origin origin2 = new Origin();
+        origin2.setCountry("New Origin 2");
         LocalDate date1 = LocalDate.of(2023, 12, 12);
         LocalDate date2 = LocalDate.of(2023, 12, 13);
-        List<Product> dummyProducts = productRepository.saveAllAndFlush(Stream.of(new Product(UUID.randomUUID(), "New Variety 1", category1, origin1, BigDecimal.valueOf(10.00), BigDecimal.valueOf(12.00), date1,  49),
-                new Product(UUID.randomUUID(), "New Variety 2", category2, origin2, BigDecimal.valueOf(11.00), BigDecimal.valueOf(13.00), date2,  8)).collect(Collectors.toList()));
+        List<Product> dummyProducts = productRepository.saveAllAndFlush(Stream.of(new Product("New Variety 1", category1, origin1, BigDecimal.valueOf(10.00), BigDecimal.valueOf(12.00), date1,  49),
+                new Product("New Variety 2", category2, origin2, BigDecimal.valueOf(11.00), BigDecimal.valueOf(13.00), date2,  8)).collect(Collectors.toList()));
 
         mvc.perform(MockMvcRequestBuilders
                         .get("/products")
